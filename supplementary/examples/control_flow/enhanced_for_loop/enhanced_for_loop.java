2cf0bcb885a74160a81d574f072f8333346f0ef0
Shawn Bailey <sbailey6@binghamton.edu>
2022-09-06 20:40:02 -0400

    one more enhanced for loop with strings

--- supplementary/examples/control_flow/enhanced_for_loop/enhanced_for_loop.java
+++ supplementary/examples/control_flow/enhanced_for_loop/enhanced_for_loop.java
@@ -2,11 +2,18 @@ class enhanced_for_loop
 {
 	public static void main(String[] args)
 	{
-		int[] arr = {11,22,33,44};
+		int[] int_arr = {11,22,33,44};
 
-		for(int i : arr)
+		for(int i : int_arr)
 		{
-			System.out.println(i);
+			System.out.println("int: " + i);
+		}
+
+		String[] str_arr = {"cat", "dog", "fish", "peacock"};
+
+		for(String str : str_arr)
+		{
+			System.out.println("String: " + str);
 		}
 	}
 }
